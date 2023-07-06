76d9fed4451d96eab2caa5801c39693140e25a49
熊明祥 <xiongmingxiang@sou.edu.cn>
2023-07-06 17:48:11 +0800

    test(root): remove log in production

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,10 @@ import { Provider } from 'react-redux'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
+if (process.env.NODE_ENV === 'production') {
+  console.log = () => {}
+}
+
 const ConfigApp: React.FC = () => {
   return (
     <Provider store={store}>
